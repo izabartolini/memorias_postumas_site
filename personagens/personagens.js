48fc60f26bab4bb290414b8266036aef0c34db7a
iza <115378274+izabartolini@users.noreply.github.com>
2023-12-12 03:30:52 -0200

    finalização do projeto
    adição de media query para todas as paginas, adição de menu responsivo

--- personagens/personagens.js
+++ personagens/personagens.js
@@ -3,6 +3,60 @@ var btQuincasBorba = document.getElementById("quincas");
 var footer = document.getElementById("footer")
 var btVoltar = document.getElementById("seta")
 var containerSeta = document.getElementById('containerseta')
+var btMenu = document.getElementById('menu')
+var btFechar = document.getElementById('fechar')
+var links = document.getElementById('links')
+var titulo = document.getElementById('titulo')
+
+function atualizarPagina(){
+    if(window.innerWidth>=800){
+        btMenu.classList.add("icon-invisivel")
+        btFechar.classList.add("icon-invisivel")
+        links.classList.add("links")
+        titulo.classList.add("titulo")
+        titulo.style.marginRight='0%'
+    }
+    if(window.innerWidth<800){
+        btMenu.classList.add('icon-visivel')
+ }
+}
+
+btMenu.addEventListener('click', function () {
+
+    if (links.classList.contains('links')) {
+        links.classList.remove('links')
+        links.classList.add('links-aberto')
+        btMenu.classList.remove('icon-visivel')
+        btMenu.classList.add('icon-invisivel')
+        btFechar.classList.remove('icon-invisivel')
+        btFechar.classList.add('icon-visivel')
+
+        titulo.style.marginRight = "-100%"
+
+    } else {
+        alert('erro')
+    }
+
+})
+
+btFechar.addEventListener('click', function(){
+    if (links.classList.contains('links-aberto')) {
+        links.classList.add('links')
+        links.classList.remove('links-aberto')
+        btMenu.classList.add('icon-visivel')
+        btMenu.classList.remove('icon-invisivel')
+        btFechar.classList.add('icon-invisivel')
+        btFechar.classList.remove('icon-visivel')
+
+        titulo.style.marginRight = "0%"
+
+    } else {
+        alert('erro')
+    }
+})
+
+window.addEventListener('resize', atualizarPagina)
+window.addEventListener('load', atualizarPagina)
 
 btBrasCubas.addEventListener("click", function () {
 
@@ -62,3 +116,6 @@ btVoltar.addEventListener("click", function () {
     }
 
 })
+
+window.addEventListener('resize', atualizarPagina)
+window.addEventListener('load', atualizarPagina)
